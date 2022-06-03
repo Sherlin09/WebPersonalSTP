2fe20c38592792666d0a8d350ae0cda78f837bd0
Sherlin Toriz Pérez <sherlintp09@gmail.com>
2022-06-02 20:20:45 -0500

    Extendiendo la app

--- core/views.py
+++ core/views.py
@@ -2,5 +2,22 @@ from django.shortcuts import render, HttpResponse
 from django.shortcuts import render
 # Create your views here.
 
+html_base = """
+    <h1>Mi Web Personal</h1>
+    <ul>
+        <li><a href="/">Portada</a></li>
+        <li><a href="/about/">Acerca de</a></li>
+    </ul>
+"""
+
 def home(request):
-    return HttpResponse("<h1>Mi Web Personal</h1><h2>Portada</h2>")
+    return HttpResponse(html_base + """
+        <h2>Bienvenidos</h2>
+        <p>Esto es la portada.</p>
+    """)
+
+def about(request):
+    return HttpResponse(html_base + """
+        <h2>Acerca de</h2>
+        <p>Me llamo Sherlin y me encanta Django!</p>
+    """)
